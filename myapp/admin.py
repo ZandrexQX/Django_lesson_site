4122ccf0870fc8ff15b7ecbb262013b9440d141c
ZandrexQX <91482671+ZandrexQX@users.noreply.github.com>
2024-03-20 06:16:23 +0300

    HW_05

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import User, Product, Order
+
+
+@admin.action(description='Сбросить в ноль')
+def reset_count(modeladmin, request, queryset):
+    queryset.update(count=0)
 
 # Register your models here.
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['name', 'email', 'phone_number']
+
+    readonly_fields = ['date_register']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'count']
+    ordering = ['name', '-price']
+    list_filter = ['date_add', 'price']
+    search_fields = ['name']
+    search_help_text = 'Поиск по полю "Наименование"'
+    actions = [reset_count]
+
+    fields = ['date_add', 'name', 'description', 'price', 'count', 'image']
+    readonly_fields = ['date_add']
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['customer', 'total_price', 'date_ordered']
+
+    readonly_fields = ['date_ordered']
--- myapp/models.py
+++ myapp/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name='Имя')
     email = models.EmailField()
-    phone_number = models.IntegerField()
-    address = models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    date_register = models.DateTimeField(auto_now_add=True)
+    phone_number = models.IntegerField(verbose_name='Телефон')
+    address = models.CharField(max_length=100, verbose_name='Адресс')
+    password = models.CharField(max_length=100, verbose_name='Пароль')
+    date_register = models.DateTimeField(auto_now_add=True, verbose_name='Дата регистрации')
 
     def __str__(self):
         return f"Username - {self.name}"
@@ -16,9 +16,9 @@ class User(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
     price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Цена')
-    description = models.TextField(verbose_name='Описание')
+    description = models.TextField(default='', blank=True, verbose_name='Описание')
     count = models.IntegerField(verbose_name='Количество')
-    image = models.ImageField(verbose_name='Вид продукта')
+    image = models.ImageField(blank=True, verbose_name='Вид продукта')
     date_add = models.DateTimeField(auto_now_add=True, verbose_name='Дата добавления')
 
     def __str__(self):
@@ -26,10 +26,10 @@ class Product(models.Model):
 
 
 class Order(models.Model):
-    customer = models.ForeignKey(User, on_delete=models.CASCADE)
-    products = models.ManyToManyField(Product)
+    customer = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Заказчик')
+    products = models.ManyToManyField(Product, verbose_name='Продукты')
     date_ordered = models.DateTimeField(auto_now_add=True)
-    total_price = models.DecimalField(max_digits=8, decimal_places=2)
+    total_price = models.DecimalField(max_digits=8, decimal_places=2, verbose_name='Итоговая цена')
 
     def __str__(self):
         return f"Order by {self.customer.name}, total: {self.total_price}. Date: {self.date_ordered}"
